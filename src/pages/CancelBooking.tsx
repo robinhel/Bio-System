ef285e5deafd6a2db3c3836fa90dc3af867dde02
Robin Hellgren <robinhellgren@Robins-MacBook-Pro.local>
2026-02-03 18:41:46 +0100

    cancelbooking css

--- src/pages/CancelBooking.tsx
+++ src/pages/CancelBooking.tsx
@@ -8,7 +8,7 @@ export default function CancelBookingPage() {
     return <>
 
         <div className="cancelbooking">
-            <h1 className="cancelbooking-title">Avboka Bokning</h1>
+            <h1 className="cancelbooking-title">Avboka</h1>
 
             <p className="cancelbooking-label">Ange din bokningskod nedan för att avboka din bokning:</p>
 
@@ -17,14 +17,16 @@ export default function CancelBookingPage() {
             <button className="cancelbooking-btn">Hämta Bokning</button>
 
             <ul id="list">
-                <p>Datum och tid</p>
+
+                <p>namn</p>
+
+                <p>Datum + tid</p>
 
                 <p>Plats</p>
 
                 <p>Bokningsnummer</p>
 
-                <p>Pris på bokning</p>
-                <p>1kr</p>
+                <p>Pris</p>
 
             </ul>
 
