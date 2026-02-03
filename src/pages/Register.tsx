1360dbbdc77077fbd4a899448137c2330d08c899
Robin Hellgren <robinhellgren@Robins-MacBook-Pro.local>
2026-02-03 10:49:43 +0100

    again

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -6,15 +6,19 @@ export default function RegisterPage() {
     return <>
         <div className="auth-page">
             <h1 className="auth-title">Skapa Konto</h1>
-            <p className="auth-label">E-post (användarnamn)</p>
+            <p className="auth-label">E-post (användarnamn) </p>
             <input
                 type="text"
+                name="email"
+                required
             />
             <p className="auth-label">Lösenord</p>
             <input
                 type="password"
+                name="password"
+                required
             />
-            
+
             <button className="auth-btn">Skapa Konto</button>
         </div>
 
