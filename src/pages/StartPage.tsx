40be6a1cbacfc12b039c368fbd0ce9bffa4f5bea
CallePalsson <carlerikuno@gmail.com>
2026-02-20 13:43:24 +0100

    Fix: Trailer loading from database
    updated database and changed code to load trailers from the database

--- src/pages/StartPage.tsx
+++ src/pages/StartPage.tsx
@@ -1,11 +1,10 @@
- import { Button } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
 import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
-interface Movie
-{
+interface Movie {
     id: number;
     Title: string;
     Description: string;
@@ -16,9 +15,9 @@ interface Movie
 
 
 StartPage.route = {
-    path: '/',         
-    menuLabel: 'Hem',  
-    index: 1           
+    path: '/',
+    menuLabel: 'Hem',
+    index: 1
 };
 
 export default function StartPage() {
@@ -27,9 +26,9 @@ export default function StartPage() {
 
     useEffect(() => {
         fetch('/api/Movies')
-        .then(res => res.json())
-        .then(data => setMovies(data))
-        .catch(error => console.error ('Fel vid hämtning av filmer:', error))
+            .then(res => res.json())
+            .then(data => setMovies(data))
+            .catch(error => console.error('Fel vid hämtning av filmer:', error))
     }, []);
 
     const filteredMovies = movies.filter(movie =>
@@ -37,27 +36,27 @@ export default function StartPage() {
     );
 
     return <>
-    <div className="start-page">
+        <div className="start-page">
             <h1 id="textabovesearchbar">Vilken film vill du se?</h1>
-        <InputGroup className="mb-3">
-            <Form.Control
+            <InputGroup className="mb-3">
+                <Form.Control
                     placeholder="Sök efter en film..."
-                
-                    value={searchTerm}
-                onChange={(e) => setSearchTerm(e.target.value)}
-                />
-        </InputGroup>
-        <div className="movies-grid">
-            {filteredMovies.map(movie => (
-             <Link to={`/movie-details/${movie.id}`} key={movie.id} className='movie-card'>
-                <img src={movie.Cover} alt={movie.Title} />
-                    <h3 className="movie-title">{movie.Title}</h3>
-                    <p className="movie-genre">{movie.Genre}</p>
-             </Link>   
-            ))}
-        </div>
 
-    </div>
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
+                />
+            </InputGroup>
+            <div className="movies-grid">
+                {filteredMovies.map(movie => (
+                    <Link to={`/movie-details/${movie.id}`} key={movie.id} className='movie-card'>
+                        <img src={movie.Cover} alt={movie.Title} />
+                        <h3 className="movie-title">{movie.Title}</h3>
+                        <p className="movie-genre">{movie.Genre}</p>
+                    </Link>
+                ))}
+            </div>
+
+        </div>
     </>;
 
 }
